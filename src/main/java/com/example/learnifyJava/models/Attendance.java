e10747577a36c43e6c0d05a1363c444c43be44ad
Santiago Loaiza <santiagoloaizamadrid@gmail.com>
2025-03-20 08:48:27 -0500

    classes completed without foreign keys

--- src/main/java/com/example/learnifyJava/models/Attendance.java
+++ src/main/java/com/example/learnifyJava/models/Attendance.java
@@ -8,4 +8,37 @@ public class Attendance {
     private Integer idAttendance;
     private Date date;
     private TypeStatusAttendance status;
+
+    public Attendance() {
+    }
+
+    public Attendance(Integer idAttendance, Date date, TypeStatusAttendance status) {
+        this.idAttendance = idAttendance;
+        this.date = date;
+        this.status = status;
+    }
+
+    public Integer getIdAttendance() {
+        return idAttendance;
+    }
+
+    public void setIdAttendance(Integer idAttendance) {
+        this.idAttendance = idAttendance;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public TypeStatusAttendance getStatus() {
+        return status;
+    }
+
+    public void setStatus(TypeStatusAttendance status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/example/learnifyJava/models/Course.java
+++ src/main/java/com/example/learnifyJava/models/Course.java
@@ -3,4 +3,28 @@ package com.example.learnifyJava.models;
 public class Course {
     private Integer idCourse;
     private String name;
+
+    public Course() {
+    }
+
+    public Course(Integer idCourse, String name) {
+        this.idCourse = idCourse;
+        this.name = name;
+    }
+
+    public Integer getIdCourse() {
+        return idCourse;
+    }
+
+    public void setIdCourse(Integer idCourse) {
+        this.idCourse = idCourse;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
